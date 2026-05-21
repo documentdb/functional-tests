6c4fc9668e4348f443dc9adb4626f4d2c5dd81a2
Alina (Xi) Li <Alina.Li@improving.com>
2026-05-21 14:16:25 -0700

    remove arity tests
    Signed-off-by: Alina (Xi) Li <Alina.Li@improving.com>

--- documentdb_tests/compatibility/tests/core/operator/accumulators/sum/test_sum_errors.py
+++ documentdb_tests/compatibility/tests/core/operator/accumulators/sum/test_sum_errors.py
@@ -1,4 +1,4 @@
-"""Tests for $sum accumulator: syntax validation, arity errors, and error propagation."""
+"""Tests for $sum accumulator: syntax validation and error propagation."""
 
 from __future__ import annotations
 
@@ -11,8 +11,6 @@ from documentdb_tests.framework.assertions import assertFailureCode
 from documentdb_tests.framework.error_codes import (
     CONVERSION_FAILURE_ERROR,
     DIVIDE_BY_ZERO_V2_ERROR,
-    EXPRESSION_OBJECT_MULTIPLE_FIELDS_ERROR,
-    GROUP_ACCUMULATOR_ARRAY_ARGUMENT_ERROR,
     INVALID_DOLLAR_FIELD_PATH,
 )
 from documentdb_tests.framework.executor import execute_command
@@ -27,48 +25,6 @@ SUM_SYNTAX_VALIDATION_TESTS: list[AccumulatorTestCase] = [
         pipeline=[{"$group": {"_id": None, "result": {"$sum": "$"}}}],
         error_code=INVALID_DOLLAR_FIELD_PATH,
         msg="$sum should reject '$' as an invalid FieldPath",
-    ),
-]
-
-# Property [Arity Errors]: array syntax is rejected in accumulator context,
-# and multi-key expression objects produce an expression parsing error.
-SUM_ARITY_ERROR_TESTS: list[AccumulatorTestCase] = [
-    AccumulatorTestCase(
-        "arity_empty_array",
-        docs=[{"v": 1}],
-        pipeline=[{"$group": {"_id": None, "result": {"$sum": []}}}],
-        error_code=GROUP_ACCUMULATOR_ARRAY_ARGUMENT_ERROR,
-        msg="$sum should reject empty array in accumulator context",
-    ),
-    AccumulatorTestCase(
-        "arity_single_element_array",
-        docs=[{"v": 1}],
-        pipeline=[{"$group": {"_id": None, "result": {"$sum": [1]}}}],
-        error_code=GROUP_ACCUMULATOR_ARRAY_ARGUMENT_ERROR,
-        msg="$sum should reject single-element array in accumulator context",
-    ),
-    AccumulatorTestCase(
-        "arity_single_field_ref_array",
-        docs=[{"v": 1}],
-        pipeline=[{"$group": {"_id": None, "result": {"$sum": ["$v"]}}}],
-        error_code=GROUP_ACCUMULATOR_ARRAY_ARGUMENT_ERROR,
-        msg="$sum should reject single field ref in array in accumulator context",
-    ),
-    AccumulatorTestCase(
-        "arity_multi_element_array",
-        docs=[{"v": 1}],
-        pipeline=[{"$group": {"_id": None, "result": {"$sum": [1, 2, 3]}}}],
-        error_code=GROUP_ACCUMULATOR_ARRAY_ARGUMENT_ERROR,
-        msg="$sum should reject multi-element array in accumulator context",
-    ),
-    AccumulatorTestCase(
-        "arity_multi_key_expression_object",
-        docs=[{"v": 1}],
-        pipeline=[
-            {"$group": {"_id": None, "result": {"$sum": {"$add": [1, 2], "$multiply": [3, 4]}}}}
-        ],
-        error_code=EXPRESSION_OBJECT_MULTIPLE_FIELDS_ERROR,
-        msg="$sum should reject multi-key expression object",
     ),
 ]
 
@@ -98,7 +54,6 @@ SUM_EXPRESSION_ERROR_DIVIDE_TESTS: list[AccumulatorTestCase] = [
 
 SUM_ERROR_TESTS = (
     SUM_SYNTAX_VALIDATION_TESTS
-    + SUM_ARITY_ERROR_TESTS
     + SUM_EXPRESSION_ERROR_PROPAGATION_TESTS
     + SUM_EXPRESSION_ERROR_DIVIDE_TESTS
 )
