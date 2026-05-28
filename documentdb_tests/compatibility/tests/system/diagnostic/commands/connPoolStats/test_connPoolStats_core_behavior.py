349e33d1537522e701ea2cc1dd03e4cc6dd94ac8
PatersonProjects <keldonhoff@gmail.com>
2026-05-28 15:30:52 -0700

    Refactored DiagnosticTestCase
    Signed-off-by: PatersonProjects <keldonhoff@gmail.com>

--- documentdb_tests/compatibility/tests/system/diagnostic/commands/connPoolStats/test_connPoolStats_core_behavior.py
+++ documentdb_tests/compatibility/tests/system/diagnostic/commands/connPoolStats/test_connPoolStats_core_behavior.py
@@ -6,7 +6,7 @@ Verifies that each response field exists and has the expected type or value.
 import pytest
 
 from documentdb_tests.compatibility.tests.system.diagnostic.utils.diagnostic_test_case import (
-    DiagnosticPropertyTest,
+    DiagnosticTestCase,
 )
 from documentdb_tests.framework.assertions import assertProperties
 from documentdb_tests.framework.executor import execute_admin_command
@@ -16,43 +16,43 @@ from documentdb_tests.framework.property_checks import Gte, IsType
 pytestmark = pytest.mark.admin
 
 
-RESPONSE_PROPERTY_TESTS: list[DiagnosticPropertyTest] = [
-    DiagnosticPropertyTest(
+RESPONSE_PROPERTY_TESTS: list[DiagnosticTestCase] = [
+    DiagnosticTestCase(
         id="totalInUse_gte_0",
         checks={"totalInUse": Gte(0)},
         msg="totalInUse should be >= 0",
     ),
-    DiagnosticPropertyTest(
+    DiagnosticTestCase(
         id="totalAvailable_gte_0",
         checks={"totalAvailable": Gte(0)},
         msg="totalAvailable should be >= 0",
     ),
-    DiagnosticPropertyTest(
+    DiagnosticTestCase(
         id="totalCreated_gte_0",
         checks={"totalCreated": Gte(0)},
         msg="totalCreated should be >= 0",
     ),
-    DiagnosticPropertyTest(
+    DiagnosticTestCase(
         id="totalRefreshing_gte_0",
         checks={"totalRefreshing": Gte(0)},
         msg="totalRefreshing should be >= 0",
     ),
-    DiagnosticPropertyTest(
+    DiagnosticTestCase(
         id="pools_is_object",
         checks={"pools": IsType("object")},
         msg="pools should be a document",
     ),
-    DiagnosticPropertyTest(
+    DiagnosticTestCase(
         id="hosts_is_object",
         checks={"hosts": IsType("object")},
         msg="hosts should be a document",
     ),
-    DiagnosticPropertyTest(
+    DiagnosticTestCase(
         id="numClientConnections_gte_0",
         checks={"numClientConnections": Gte(0)},
         msg="numClientConnections should be >= 0",
     ),
-    DiagnosticPropertyTest(
+    DiagnosticTestCase(
         id="numAScopedConnections_gte_0",
         checks={"numAScopedConnections": Gte(0)},
         msg="numAScopedConnections should be >= 0",
