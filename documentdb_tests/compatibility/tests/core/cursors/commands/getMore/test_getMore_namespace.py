ecf3a8b7df0d1daa3b4c2c8a57663958ff3ce08e
Daniel Frankcom <frankcom@amazon.com>
2026-06-02 14:34:25 -0700

    Remove unnecessary parameterization Signed-off-by: Daniel Frankcom <frankcom@amazon.com>

--- documentdb_tests/compatibility/tests/core/cursors/commands/getMore/test_getMore_namespace.py
+++ documentdb_tests/compatibility/tests/core/cursors/commands/getMore/test_getMore_namespace.py
@@ -2,47 +2,29 @@
 
 from __future__ import annotations
 
-import pytest
-
 from documentdb_tests.compatibility.tests.core.cursors.commands.utils.cursor_test_case import (
-    CursorCommandContext,
-    CursorCommandTestCase,
     open_find_cursors,
 )
 from documentdb_tests.framework.assertions import assertResult
 from documentdb_tests.framework.error_codes import UNAUTHORIZED_ERROR
 from documentdb_tests.framework.executor import execute_command
-from documentdb_tests.framework.parametrize import pytest_params
 from documentdb_tests.framework.property_checks import Eq, Len
+
 
 # Property [Namespace Match Success]: getMore succeeds when the collection
 # parameter exactly matches the find cursor's bound namespace.
-GETMORE_NAMESPACE_MATCH_FIND_TESTS: list[CursorCommandTestCase] = [
-    CursorCommandTestCase(
-        "match_find",
-        cursor_count=1,
-        find_batch_size=2,
-        command=lambda ctx: {"getMore": ctx.cursors[0], "collection": ctx.collection},
-        expected={"ok": Eq(1.0), "cursor": {"nextBatch": Len(3)}},
-        msg="getMore should succeed when collection matches find cursor namespace",
-    ),
-]
-
-
-@pytest.mark.parametrize("test_case", pytest_params(GETMORE_NAMESPACE_MATCH_FIND_TESTS))
-def test_getMore_namespace_match_find(collection, test_case: CursorCommandTestCase):
+def test_getMore_namespace_match_find(collection):
     """Test getMore succeeds with exact collection name match for find cursor."""
     collection.insert_many([{"_id": i} for i in range(5)])
-    cursors = open_find_cursors(
-        collection, test_case.cursor_count, batch_size=test_case.find_batch_size
+    (cursor_id,) = open_find_cursors(collection, 1, batch_size=2)
+    result = execute_command(
+        collection,
+        {"getMore": cursor_id, "collection": collection.name},
     )
-    ctx = CursorCommandContext.from_collection(collection, cursors=cursors)
-    result = execute_command(collection, test_case.build_command(ctx))
     assertResult(
         result,
-        expected=test_case.expected,
-        error_code=test_case.error_code,
-        msg=test_case.msg,
+        expected={"ok": Eq(1.0), "cursor": {"nextBatch": Len(3)}},
+        msg="getMore should succeed when collection matches find cursor namespace",
         raw_res=True,
     )
 
