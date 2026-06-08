c38826e0ca1fa8524f9306261f9735fc0edfb184
Alina (Xi) Li <Alina.Li@improving.com>
2026-06-08 12:57:07 -0700

    fix according to style guide
    Signed-off-by: Alina (Xi) Li <Alina.Li@improving.com>

--- documentdb_tests/compatibility/tests/core/sessions/commands/commitTransaction/test_commitTransaction_comment.py
+++ documentdb_tests/compatibility/tests/core/sessions/commands/commitTransaction/test_commitTransaction_comment.py
@@ -20,170 +20,163 @@ from documentdb_tests.framework.assertions import assertSuccessPartial
 from documentdb_tests.framework.executor import execute_session_command
 from documentdb_tests.framework.parametrize import pytest_params
 
-pytestmark = [pytest.mark.admin, pytest.mark.replica_set]
-
-_INSERT_OP = [SessionOperation(op=SessionOp.INSERT, document={"_id": 1})]
-_OK_RESPONSE = {"ok": 1.0}
-
-
-# ---------------------------------------------------------------------------
 # Property [comment Type Acceptance]: comment accepts any BSON type.
-# ---------------------------------------------------------------------------
-
 COMMENT_TYPE_TESTS: list[SessionTestCase] = [
     SessionTestCase(
         "comment_string",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": "test comment"},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:string",
     ),
     SessionTestCase(
         "comment_string_empty",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": ""},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:empty string",
     ),
     SessionTestCase(
         "comment_int32",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": 42},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:int32",
     ),
     SessionTestCase(
         "comment_int64",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": Int64(42)},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:Int64",
     ),
     SessionTestCase(
         "comment_double",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": 3.14},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:double",
     ),
     SessionTestCase(
         "comment_decimal128",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": Decimal128("1.5")},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:Decimal128",
     ),
     SessionTestCase(
         "comment_bool_true",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": True},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:true",
     ),
     SessionTestCase(
         "comment_bool_false",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": False},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:false",
     ),
     SessionTestCase(
         "comment_null",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": None},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:null",
     ),
     SessionTestCase(
         "comment_object",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": {"key": "value"}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:object",
     ),
     SessionTestCase(
         "comment_object_empty",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": {}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:empty object",
     ),
     SessionTestCase(
         "comment_array",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": [1, 2, 3]},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:array",
     ),
     SessionTestCase(
         "comment_array_empty",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": []},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:empty array",
     ),
     SessionTestCase(
         "comment_objectid",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": ObjectId()},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:ObjectId",
     ),
     SessionTestCase(
         "comment_datetime",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={
             "commitTransaction": 1,
             "comment": datetime(2024, 1, 1, tzinfo=timezone.utc),
         },
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:datetime",
     ),
     SessionTestCase(
         "comment_binary",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": Binary(b"\x00")},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:Binary",
     ),
     SessionTestCase(
         "comment_regex",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": Regex(".*")},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:Regex",
     ),
     SessionTestCase(
         "comment_timestamp",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": Timestamp(0, 0)},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:Timestamp",
     ),
     SessionTestCase(
         "comment_minkey",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": MinKey()},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:MinKey",
     ),
     SessionTestCase(
         "comment_maxkey",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": MaxKey()},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:MaxKey",
     ),
     SessionTestCase(
         "comment_code",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "comment": Code("function(){}")},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept comment:Code",
     ),
 ]
 
 
+@pytest.mark.admin
+@pytest.mark.replica_set
 @pytest.mark.parametrize("test", pytest_params(COMMENT_TYPE_TESTS))
 def test_commitTransaction_comment(collection, test):
     """Test commitTransaction comment parameter type acceptance in a transaction."""
--- documentdb_tests/compatibility/tests/core/sessions/commands/commitTransaction/test_commitTransaction_writeconcern.py
+++ documentdb_tests/compatibility/tests/core/sessions/commands/commitTransaction/test_commitTransaction_writeconcern.py
@@ -19,235 +19,213 @@ from documentdb_tests.framework.assertions import assertSuccessPartial
 from documentdb_tests.framework.executor import execute_session_command
 from documentdb_tests.framework.parametrize import pytest_params
 
-pytestmark = [pytest.mark.admin, pytest.mark.replica_set]
-
-_INSERT_OP = [SessionOperation(op=SessionOp.INSERT, document={"_id": 1})]
-_OK_RESPONSE = {"ok": 1.0}
-
-
-# ---------------------------------------------------------------------------
 # Property [writeConcern Document Acceptance]: writeConcern accepts document values.
-# ---------------------------------------------------------------------------
-
 WRITECONCERN_ACCEPTANCE_TESTS: list[SessionTestCase] = [
     SessionTestCase(
         "writeconcern_empty_doc",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept empty writeConcern document",
     ),
     SessionTestCase(
         "writeconcern_null",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": None},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern:null",
     ),
     SessionTestCase(
         "wc_combined_w_j_wtimeout",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={
             "commitTransaction": 1,
             "writeConcern": {"w": "majority", "j": True, "wtimeout": 10_000},
         },
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept combined w + j + wtimeout",
     ),
     SessionTestCase(
         "wc_w0_j_true",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"w": 0, "j": True}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept conflicting w:0 with j:true",
     ),
     SessionTestCase(
         "wc_fsync_true",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"fsync": True}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept legacy writeConcern.fsync:true",
     ),
 ]
 
-
-# ---------------------------------------------------------------------------
 # Property [w Accepted Values]: w accepts int and string "majority" values.
-# ---------------------------------------------------------------------------
-
 W_ACCEPTANCE_TESTS: list[SessionTestCase] = [
     SessionTestCase(
         "w_int32_one",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"w": 1}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.w:1",
     ),
     SessionTestCase(
         "w_int32_zero",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"w": 0}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.w:0 (unacknowledged)",
     ),
     SessionTestCase(
         "w_majority",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"w": "majority"}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.w:'majority'",
     ),
     SessionTestCase(
         "w_int64",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"w": Int64(1)}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.w:Int64(1)",
     ),
     SessionTestCase(
         "w_double_whole",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"w": 1.0}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.w:1.0",
     ),
     SessionTestCase(
         "w_double_fractional",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"w": 1.5}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.w:1.5",
     ),
     SessionTestCase(
         "w_decimal128",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"w": Decimal128("1")}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.w:Decimal128('1')",
     ),
 ]
 
-
-# ---------------------------------------------------------------------------
 # Property [j Accepted Values]: j accepts boolean and numeric types.
-# ---------------------------------------------------------------------------
-
 J_ACCEPTANCE_TESTS: list[SessionTestCase] = [
     SessionTestCase(
         "j_bool_true",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"j": True}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.j:true",
     ),
     SessionTestCase(
         "j_bool_false",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"j": False}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.j:false",
     ),
     SessionTestCase(
         "j_int32_one",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"j": 1}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.j:1 (coerced to true)",
     ),
     SessionTestCase(
         "j_int32_zero",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"j": 0}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.j:0 (coerced to false)",
     ),
     SessionTestCase(
         "j_null",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"j": None}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.j:null",
     ),
 ]
 
-
-# ---------------------------------------------------------------------------
 # Property [wtimeout Accepted Values]: wtimeout accepts numeric types broadly.
-# ---------------------------------------------------------------------------
-
 WTIMEOUT_ACCEPTANCE_TESTS: list[SessionTestCase] = [
     SessionTestCase(
         "wtimeout_int32_positive",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"wtimeout": 1000}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.wtimeout:1000",
     ),
     SessionTestCase(
         "wtimeout_int32_zero",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"wtimeout": 0}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.wtimeout:0 (no timeout)",
     ),
     SessionTestCase(
         "wtimeout_int64",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"wtimeout": Int64(1000)}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.wtimeout:Int64(1000)",
     ),
     SessionTestCase(
         "wtimeout_double_whole",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"wtimeout": 1000.0}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.wtimeout:1000.0",
     ),
     SessionTestCase(
         "wtimeout_negative",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"wtimeout": -1}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.wtimeout:-1",
     ),
     SessionTestCase(
         "wtimeout_string",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"wtimeout": "1000"}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.wtimeout:'1000'",
     ),
     SessionTestCase(
         "wtimeout_bool",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"wtimeout": True}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.wtimeout:true",
     ),
     SessionTestCase(
         "wtimeout_null",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"wtimeout": None}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.wtimeout:null",
     ),
     SessionTestCase(
         "wtimeout_object",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"wtimeout": {}}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.wtimeout:{}",
     ),
     SessionTestCase(
         "wtimeout_array",
-        ops=_INSERT_OP,
+        ops=[SessionOperation(op=SessionOp.INSERT, document={"_id": 1})],
         commit_command={"commitTransaction": 1, "writeConcern": {"wtimeout": []}},
-        expected_response=_OK_RESPONSE,
+        expected_response={"ok": 1.0},
         msg="commitTransaction should accept writeConcern.wtimeout:[]",
     ),
 ]
-
 
 WRITECONCERN_TESTS: list[SessionTestCase] = (
     WRITECONCERN_ACCEPTANCE_TESTS
@@ -257,6 +235,8 @@ WRITECONCERN_TESTS: list[SessionTestCase] = (
 )
 
 
+@pytest.mark.admin
+@pytest.mark.replica_set
 @pytest.mark.parametrize("test", pytest_params(WRITECONCERN_TESTS))
 def test_commitTransaction_writeconcern(collection, test):
     """Test commitTransaction writeConcern parameter acceptance in a transaction."""
