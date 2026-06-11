77487c00a5590738f9879de32f1665a874fb352f
Daniel Frankcom <frankcom@amazon.com>
2026-06-11 12:23:56 -0700

    Remove remaining ignore_doc_order Signed-off-by: Daniel Frankcom <frankcom@amazon.com>

--- documentdb_tests/compatibility/tests/core/operator/stages/test_stages_position_redact.py
+++ documentdb_tests/compatibility/tests/core/operator/stages/test_stages_position_redact.py
@@ -126,5 +126,4 @@ def test_stage_position_redact_cases(collection, test_case: StageTestCase):
         expected=test_case.expected,
         error_code=test_case.error_code,
         msg=test_case.msg,
-        ignore_doc_order=True,
     )
